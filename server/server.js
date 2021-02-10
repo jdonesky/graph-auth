8b84f6c913145cc31cb5357d9843df14c74448f7
Jon <jdonesky@gmail.com>
2021-02-10 16:12:21 -0500

    base commit - cloned compatibility layer between passport and graphql

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://jdonesky:Bpsqs3bdv05epgJA@cluster0.9aclf.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
